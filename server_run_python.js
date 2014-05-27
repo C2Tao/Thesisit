18584468b1d393b5fae8b22af4f0b73cb208d741
C2Tao <b97901182@gmail.com>
2014-05-27 22:48:47 +0800

    query server

--- server_run_python.js
+++ server_run_python.js
@@ -1,30 +1,53 @@
 var express = require('express'); 
 var app = express();
 var fs = require('fs');
+var jsonfile = __dirname + '/json_all.txt';
+
+
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(app.router);
 //app.use(express.static(__dirname + '/'));
 
-app.get('/demo_json', function(req, res){
+app.get('/query/:querystring', function(req, res){
      
      var python = require('child_process').spawn(
        'python',
        // second argument is array of parameters, e.g.:
-       [__dirname + "/generate_json.py"] //this parameter is the script file you want to run
+       [__dirname + "/generate_json.py", req.params.querystring] //this parameter is the script file you want to run
      );
+
+
      var output = "";
-     
      //when python dump info to standard output(e.g.:print ...), it would call this callback
      python.stdout.on('data', function(data){ 
         output += data;
      });
+
+
      
-     //when python finished running the script
+      
+
      python.on('close', function(code){ 
-    
        //I think here we can read from the json file that we saved in python script
+       
+      fs.readFile(jsonfile, 'utf8', function (err, data) {
+        if (err) {
+          console.log('Error: ' + err);
+          return;
+        }
+       
+        data = JSON.parse(data);
+       
+        console.dir(data);
+
+        res.json(data);
+      });     //when python finished running the script
+
+
+
+
 
      });
          
